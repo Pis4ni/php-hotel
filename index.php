777e9341159b1a35b9e7d107eafa09ed2dca5d30
Pis4ni <pis4wrk@gmail.com>
2023-09-26 17:13:05 +0200

    add: styled table bootstrap

--- index.php
+++ index.php
@@ -37,6 +37,41 @@ $hotels = [
         'vote' => 2,
         'distance_to_center' => 50
     ],
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
 
 ];
 
@@ -50,40 +85,58 @@ $hotels = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>php</title>
 </head>
 
 <body>
-    <ul>
-        <?php foreach ($hotels as $hotel): ?>
-            <li>
-                <!-- title -->
-                <h2>
-                    <?php echo $hotel['name'] ?>
-                </h2>
-                <hr>
-                <!-- description -->
-                <span>
-                    <?php echo $hotel['description'] ?>
-                </span>
-                <hr>
-                <!-- has park? -->
-                <span>
-                    <?php echo $hotel['vote'] ?>
-                </span>
-                <hr>
-                <!-- vote -->
-                <span>
-                    <?php echo $hotel['vote'] ?>
-                </span>
-                <hr>
-                <!-- distance -->
-                <span>
-                    <?php echo $hotel['distance_to_center'] ?> 
-                </span>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+
+    <div class="container m-5">
+
+        <table class="table table-dark table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+
+                    <?php foreach ($hotels as $key => $hotel): ?>
+                        <?php $keys = array_keys($hotel); ?>
+                    <?php endforeach; ?>
+
+                    <?php foreach ($keys as $keyH => $hotelElements): ?>
+                        <th scope="col">
+                            <?php echo ($keys[$keyH]) ?>
+                        </th>
+                    <?php endforeach; ?>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php foreach ($hotels as $key => $hotel): ?>
+                    <tr>
+                        <th scope="row">
+                            <?php echo (array_keys($hotels)[$key]) ?>
+                        </th>
+
+                        <?php foreach ($hotel as $keyC => $hotelDetail): ?>
+                            <?php if ($keyC == 'parking' && $hotelDetail == 1): ?>
+                                <th scope="col">
+                                    <?php echo ('Avaible') ?>
+                                </th>
+                            <?php else: ?>
+                                <th scope="col">
+                                    <?php echo ($hotelDetail) ?>
+                                </th>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+
+
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
